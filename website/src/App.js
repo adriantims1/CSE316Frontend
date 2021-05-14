b7f138de8efb2ec39ddb8e5f6165847cff07c24e
Adrian Sowandi <adrian.sowandi@stonybrook.edu>
2021-05-14 10:18:50 +0900

    Navbar component created

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/NavBar";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fdfeff",
+      dark: "#f7f9fb",
+    },
+    secondary: {
+      light: "#B0D2E8",
+      main: "#0b4870",
+    },
+  },
+});
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div className="App">
         <Navbar />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
--- website/src/components/NavBar.js
+++ website/src/components/NavBar.js
@@ -1,23 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   AppBar,
   Toolbar,
   Grid,
   makeStyles,
   withStyles,
+  Box,
+  Link,
+  Button,
 } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   navBar: {},
   navbarGrid: {
     justifyContent: "space-between",
   },
   navLink: {
-    fontFamily: "Comfortaa",
-    color: "#0b4870",
-    fontSize: "24px",
+    fontFamily: "Comfortaa ",
+    color: theme.palette.secondary.main,
+    fontSize: "16px",
+    margin: theme.spacing(3),
   },
-});
+  navContainer: {
+    alignItems: "center",
+  },
+}));
+
+const StyledButton = withStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.secondary.main,
+    color: "white",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+}))(Button);
 
 export default function Navbar() {
   const classes = useStyles();
@@ -32,10 +49,27 @@ export default function Navbar() {
         <Toolbar>
           <Grid container className={classes.navbarGrid}>
             <Grid item className={classes.navLink}>
-              logo
+              <img
+                src="https://res.cloudinary.com/dtkgfy2wk/image/upload/v1620954709/logo_fuhdgl.svg"
+                style={{ height: "50px", maxWidth: "75px" }}
+                alt=""
+              ></img>
             </Grid>
-            <Grid item className={classes.navLink}>
-              navbar
+            <Grid item>
+              <Box display="flex" className={classes.navContainer}>
+                <Link className={classes.navLink}>Home</Link>
+                <Link className={classes.navLink}>About</Link>
+                <Link className={classes.navLink}>Live Trades</Link>
+                <Link className={classes.navLink}>Contact</Link>
+                <StyledButton
+                  variant="contained"
+                  className={classes.navLink}
+                  color="secondary"
+                  size="medium"
+                >
+                  Login
+                </StyledButton>
+              </Box>
             </Grid>
           </Grid>
         </Toolbar>
