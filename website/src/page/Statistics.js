ae852910394b4f443a7a6fa5d3d6e2b6167e723e
nilson01 <nilson.chapagain@stonybrook.edu>
2021-05-31 18:11:59 +0900

    dummy graphs added

--- website/src/page/Statistics.js
+++ website/src/page/Statistics.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Sidebar from "../components/SideBar";
 import UserHeader from "../components/userHeader";
-
+import Chart from "react-apexcharts";
+import Masonry from 'react-masonry-css';
 // import { postMessagesAPIMethod } from "../api/adminClient";
 
 import {
@@ -40,13 +41,51 @@ export default function ContactUserVAdmin() {
   const localUser = JSON.parse(localStorage.getItem("isAdmin"));
   const [isAdmin, setIsAdmin] = useState(localUser);
   const classes = styles();
+
+  //BAR graph
+  const [optionsB, setOptions] = useState({ chart: { id: "basic-bar" }, xaxis: { categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998] } });
+  const [seriesB, setSeries] = useState([{ name: "series-1", data: [30, 40, 45, 50, 49, 60, 70, 91] }]);
+
+  //DONUT graph
+  const [optionsD, setOptionsD] = useState({});
+  const [seriesD, setSeriesD] = useState([44, 55, 41, 17, 15]);
+  const [labelsD, setLabelsD] = useState(['A', 'B', 'C', 'D', 'E']);
+  // const breakpoints = {
+  //   default: 3,
+  //   1100: 3,
+  //   300: 1
+  // };
+
+  const breakpoints = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+
   return (
     <Grid container alignItems="center" className={classes.root}>
       <Sidebar />
       <Grid item className={classes.rightbar}>
         <Paper className={classes.rightContainer}>
           <UserHeader page=" Statistics" />
-          {isAdmin ? "Admin": "USER"}
+
+          <Masonry
+            breakpointCols={breakpoints}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column">
+            {isAdmin ? "Admin" : "USER"}
+            <Chart
+              options={optionsB}
+              series={seriesB}
+              type="bar"
+              width="500"
+            />
+            <Chart options={optionsD} series={seriesD} type="donut" width="380" />
+
+          </Masonry>
+
+
         </Paper>
       </Grid>
     </Grid>
