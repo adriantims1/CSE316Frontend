e9f81c778db0a83d3d59fd2f8647fdf8bc8d3682
Adrian Sowandi <adrian.sowandi@stonybrook.edu>
2021-06-04 12:36:46 +0900

    bugs fixes

--- website/src/page/Live Trades.js
+++ website/src/page/Live Trades.js
@@ -37,42 +37,68 @@ const styles = makeStyles((theme) => ({
 }));
 
 const Live = () => {
-  const [transHistory, setTransHistory] = useState([]);
   //const [price, setCurrPrice] = useState(0);
-  const fetchBinProfile = async () => {
-    try {
-      var temp = await axios.get("/api/binProfile/");
-      temp = temp.data.data;
-      console.log(temp);
-      setTransHistory(temp);
-    } catch (err) {
-      alert(err);
-    }
-  };
+
   useEffect(() => {
     var a = setInterval(async () => {
       var temp = await axios.get("/api/candles/");
       temp = temp.data.data;
-
+      var temp2 = [];
+      var resMax,
+        resMin = 0;
+      console.log(temp);
+      temp.forEach((el) => {
+        var result = [el.open, el.high, el.low, el.close];
+        temp2.push({
+          x: el.created_at,
+          y: result,
+        });
+        if (resMax < Math.max(...result)) resMax = Math.max(...result);
+        if (resMin === 0 || resMin > Math.min(...result))
+          resMin = Math.min(...result);
+      });
       ApexCharts.exec("realtime", "updateSeries", [
         {
-          data: temp,
+          data: temp2,
         },
       ]);
+      ApexCharts.exec("realtime", "updateOptions", {
+        yaxis: { min: resMin, max: resMax },
+      });
     }, 5000);
-    fetchBinProfile();
-    var b = setInterval(() => {
-      fetchBinProfile();
-    }, 60000);
+
     return () => {
       clearInterval(a);
-      clearInterval(b);
     };
   }, []);
-  const [state] = useState({
+  const [candleData, setCandleData] = useState([
+    {
+      x: new Date(1538778600000),
+      y: [6629.81, 6650.5, 6623.04, 6633.33],
+    },
+    {
+      x: new Date(1538780400000),
+      y: [6632.01, 6643.59, 6620, 6630.11],
+    },
+    {
+      x: new Date(1538782200000),
+      y: [6630.71, 6648.95, 6623.34, 6635.65],
+    },
+    {
+      x: new Date(1538784000000),
+      y: [6635.65, 6651, 6629.67, 6638.24],
+    },
+    {
+      x: new Date(1538785800000),
+      y: [6638.24, 6640, 6620, 6624.47],
+    },
+  ]);
+
+  const [state, setState] = useState({
     series: [
       {
-        data: [0, 0, 0, 0, 0],
+        name: "series-1",
+        data: candleData,
       },
     ],
     options: {
@@ -81,7 +107,7 @@ const Live = () => {
         foreColor: "#fff",
         id: "realtime",
         height: 350,
-        type: "line",
+        type: "candle",
         animations: {
           enabled: true,
           easing: "linear",
@@ -115,6 +141,10 @@ const Live = () => {
 
       legend: {
         show: false,
+      },
+      yaxis: {
+        max: 641.8685016,
+        min: 641.8684983,
       },
     },
   });
@@ -163,46 +193,10 @@ const Live = () => {
             <Chart
               options={state.options}
               series={state.series}
-              type="line"
+              type="candlestick"
               width="100%"
               height="100%"
             />
-          </Grid>
-        </Grid>
-        <Grid container direction="column" className={classes.rightContainer}>
-          <Grid item>
-            <Box display="flex">
-              <Typography className={classes.predictor} color="secondary">
-                Result
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid
-            item
-            style={{
-              width: "100%",
-              height: "88.5%",
-            }}
-          >
-            <Box
-              display="flex"
-              style={{
-                flexDirection: "column",
-                justifyContent: "space-between",
-                marginTop: "2vh",
-                height: "100%",
-              }}
-            >
-              {transHistory.map((el) => (
-                <ResultCard
-                  key={el.id}
-                  amount={el.amount}
-                  win={el.win}
-                  trend={el.trend}
-                  status={el.status}
-                />
-              ))}
-            </Box>
           </Grid>
         </Grid>
       </Paper>
